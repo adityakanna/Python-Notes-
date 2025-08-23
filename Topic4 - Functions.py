ee532d0b2f6222ec6c2a2b805c7beaee7496a52a
AdityaKanna <156410706+adityakanna@users.noreply.github.com>
2025-08-23 14:31:15 +0530

    Topic 4 - Functions

---
+++ Topic4 - Functions.py
@@ -0,0 +1,66 @@
+'''
+-> Function(): 1. Set of statements which are used to perform specific operation o variable are called
+               as function.
+               2. we can declare function by using 'def' Keyword.
+'''
+
+'''
+They are 2 types of functions: 
+1. Pre-define Function: a. If function are already available then we will called as Pre - defined function.
+   b. To Execute function Logic we need to call function by using function name.
+Ex: print(), Input(), type(), min(), max(),....
+'''
+# 1. a = '5'
+#    print(type(a))
+
+# 2. a = [5, 10, 25, 8, 3, 15]
+#    print('Total no.of Element are',len(a)]
+#    print('Sum of Element are',sum(a)]
+#    print('Smallest of Element are',min(a)]
+#    print('Larger of Element are',max(a)]
+
+'''
+2. User - define Function: a. Based on application requirments if you create any function then it is called
+                           as user - define function.
+b. For user - define Function, we have to perform 2 things:-
+   1. Declare Function by using ('def') keyword.
+   2. Call Function by using function name.
+'''
+# Syntax to Declare a Function :-
+# Ex1: def Show()
+#         print('Hai')
+#      print('Thank you')
+#      show()
+# Output: Hai
+#         Thank you
+
+# Ex2: def show()
+#         print('Hai')
+#      print('Thank You')
+#      show()
+# Output: Thank You
+#         Hai
+
+# Ex3: def withdraw()
+#          print('Hai')
+#      def balEnq()
+#          print('Hello')
+#      withdraw()
+# Output: Hai
+
+'''
+Functions with Parameters: 
+1. If you have parameters than at the time of calling function we need to pass value
+Ex: def Login (Uname, pwd)
+        print(Uname)
+        print(pwd)
+    Login('Venkat','A123a')
+Ex: def converstion(n)
+         D = Float(input('Enter the One Doller into Rupees')
+         C = n/D
+         print(c)
+    a = Float(input('Enter the Amount Value'))
+    converstion(a)
+'''
+
+
